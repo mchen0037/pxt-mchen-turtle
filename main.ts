518738b4fc99e8741849ec3672a86cc14aeab65a
Mighty Chen <mchen73@ucmerced.edu>
2020-01-08 11:39:49 -0500

    init with example

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
-basic.showLeds(`
+//% weight=100 color=#4DA3FF icon="\uf437"
+namespace lame {
+
+  //% blockId=dancingBlock block="dancing %steps"
+  //% weight=98 blockGap=8
+  export function dancing(steps: number): void {
+    basic.showLeds(`
     . . . . .
     . # . # .
     . . . . .
-    # . . . #
     . # # # .
-    `);
+    . . . . .
+    `)
+  }
+
+}
