4b6179f8757e365fa250fdcf2cdc5b37d2d318d6
Mighty Chen <31076823+mchen0037@users.noreply.github.com>
2020-01-19 10:13:58 -0800

    added code to skeleton

--- main.ts
+++ main.ts
@@ -1,99 +1,123 @@
 //% weight=100 color=#4DA3FF icon="\uf437"
 namespace turtle {
 
-  //% blockId=dancingBlock block="go forward %steps|steps"
-  //% weight=98 blockGap=8
-  export function forward(steps: number): void {
-    basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    . # # # .
-    . . . . .
-    `)
+    export function motorStop(): void {
+        pins.analogWritePin(AnalogPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.analogWritePin(AnalogPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+    }
 
-  }
+    export function motorForward(): void {
+        pins.analogWritePin(AnalogPin.P1, 600)
+        pins.digitalWritePin(DigitalPin.P12, 1)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.analogWritePin(AnalogPin.P2, 600)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
+    }
 
-  //% blockId=backBlock block="go back %steps|steps"
-  //% weight=98 blockGap=8
-  export function back(steps: number): void {
-    basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    . # # # .
-    . . . . .
-    `)
+    export function motorBack(): void {
+        pins.analogWritePin(AnalogPin.P1, 600)
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.digitalWritePin(DigitalPin.P13, 1)
+        pins.analogWritePin(AnalogPin.P2, 600)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+    }
 
-  }
+    export function motorRight(): void {
+        pins.analogWritePin(AnalogPin.P1, 600)
+        pins.digitalWritePin(DigitalPin.P12, 1)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.analogWritePin(AnalogPin.P2, 600)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+    }
 
-  //% blockId=rightTurnDegreesBlock block="turn right by %degrees|degrees"
-  //% weight=98 blockGap=8
-  export function rightTurnDegrees(degrees: number): void {
-    basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    . # # # .
-    . . . . .
-    `)
-  }
+    export function motorLeft(): void {
+        pins.analogWritePin(AnalogPin.P1, 600)
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.digitalWritePin(DigitalPin.P13, 1)
+        pins.analogWritePin(AnalogPin.P2, 600)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
+    }
 
-  //% blockId=rightTurnBlock block="turn right"
-  //% weight=98 blockGap=8
-  export function rightTurn(): void {
-    rightTurnDegrees(90)
-  }
+    //% blockId=dancingBlock block="go forward %steps|steps"
+    //% weight=98 blockGap=8
+    export function forward(steps: number): void {
+        motorForward()
+        basic.pause(500 * steps * steps)
+        motorStop()
+    }
 
-  //% blockId=leftTurnDegreesBlock block="turn left by %degrees|degrees"
-  //% weight=98 blockGap=8
-  export function leftTurnDegrees(degrees: number): void {
-    basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    . # # # .
-    . . . . .
-    `)
+    //% blockId=backBlock block="go back %steps|steps"
+    //% weight=98 blockGap=8
+    export function back(steps: number): void {
+        motorBack()
+        basic.pause(500 * steps)
+        motorStop()
+    }
 
-  }
+    //% blockId=rightTurnDegreesBlock block="turn right by %degrees|degrees"
+    //% weight=98 blockGap=8
+    export function rightTurnDegrees(degrees: number): void {
+        motorRight()
+        basic.pause(10 * degrees)
+        motorStop()
+    }
 
-  //% blockId=leftTurnBlock block="turn left"
-  //% weight=98 blockGap=8
-  export function leftTurn(degrees: number): void {
-    basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    . # # # .
-    . . . . .
-    `)
-  }
+    //% blockId=rightTurnBlock block="turn right"
+    //% weight=98 blockGap=8
+    export function rightTurn(): void {
+        rightTurnDegrees(90)
+    }
 
-  //% blockId=penUpBlock block="pen up"
-  //% weight=98 blockGap=8
-  export function penUp(): void {
-    basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    . # # # .
-    . . . . .
-    `)
+    //% blockId=leftTurnDegreesBlock block="turn left by %degrees|degrees"
+    //% weight=98 blockGap=8
+    export function leftTurnDegrees(degrees: number): void {
+        motorLeft()
+        basic.pause(10 * degrees)
+        motorStop()
+    }
 
-  }
+    //% blockId=leftTurnBlock block="turn left"
+    //% weight=98 blockGap=8
+    export function leftTurn(): void {
+        leftTurnDegrees(90)
+    }
 
-  //% blockId=penDownBlock block="pen down"
-  //% weight=98 blockGap=8
-  export function penDown(): void {
-    basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    . # # # .
-    . . . . .
-    `)
+    //% blockId=penUpBlock block="pen up"
+    //% weight=98 blockGap=8
+    export function penUp(): void {
+        pins.servoWritePin(AnalogPin.P12, 0)
+    }
 
-  }
-
+    //% blockId=penDownBlock block="pen down"
+    //% weight=98 blockGap=8
+    export function penDown(): void {
+        pins.servoWritePin(AnalogPin.P12, 180)
+    }
 }
+pins.digitalWritePin(DigitalPin.P14, 1)
+
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
+    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
+        turtle.motorForward()
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_DOWN) {
+        turtle.motorBack()
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_DOWN) {
+        turtle.motorLeft()
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_DOWN) {
+        turtle.motorRight()
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_UP || control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_UP || control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_UP || control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_UP) {
+        turtle.motorStop()
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
+        turtle.penUp()
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_DOWN) {
+        turtle.penDown()
+    }
+})
